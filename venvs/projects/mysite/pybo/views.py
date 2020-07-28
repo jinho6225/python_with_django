41dc71abe9d577f8168e5eaa5b146835146f8fec
jinho6225 <jinho6225@hotmail.com>
2020-07-28 13:34:39 -0700

    template_list, tamplate_detail

--- venvs/projects/mysite/pybo/views.py
+++ venvs/projects/mysite/pybo/views.py
@@ -1,11 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 
 # ---------------------------------------- [edit] ---------------------------------------- #
 from django.http import HttpResponse
-
+from .models import Question
+# ---------------------------------------------------------------------------------------- #
 
 def index(request):
-    return HttpResponse("안녕하세요 pybo에 오신것을 환영합니다.")
+# ---------------------------------------- [edit] ---------------------------------------- #
+    question_list = Question.objects.order_by('-create_date')
+    context = {'question_list': question_list}
+
+    # return HttpResponse("안녕하세요 pybo에 오신것을 환영합니다.")
+
+    return render(request, 'pybo/question_list.html', context)
+# ---------------------------------------------------------------------------------------- #
+
+def detail(request, question_id):
+# ---------------------------------------- [edit] ---------------------------------------- #
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
+
+    # return HttpResponse("안녕하세요 pybo에 오신것을 환영합니다.")
+    
+    return render(request, 'pybo/question_detail.html', context)
 # ---------------------------------------------------------------------------------------- #
