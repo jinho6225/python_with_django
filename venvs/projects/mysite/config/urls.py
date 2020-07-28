4915a4ffd0b5b40ad5cc62a2eaaad44f9865f727
jinho6225 <jinho6225@hotmail.com>
2020-07-28 00:10:34 -0700

    startapp pybo

--- venvs/projects/mysite/config/urls.py
+++ venvs/projects/mysite/config/urls.py
@@ -14,8 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+# ---------------------------------------- [edit] ---------------------------------------- #
+from pybo import views
+# ---------------------------------------------------------------------------------------- #
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+        # ---------------------------------------- [edit] ---------------------------------------- #
+    # path('pybo/', views.index),
+    path('pybo/', include('pybo.urls')),
+
+    # ---------------------------------------------------------------------------------------- #
 ]
