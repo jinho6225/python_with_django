9edfb268562714817697b81c6650aa0a569bbe36
jinho6225 <jinho6225@hotmail.com>
2020-07-28 16:26:42 -0700

    added namespace in url

--- venvs/projects/mysite/pybo/urls.py
+++ venvs/projects/mysite/pybo/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 
 from . import views
 
+app_name = 'pybo'
+
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name="index"),
     # ---------------------------------------- [edit] ---------------------------------------- #
-    path('<int:question_id>/', views.detail)
+    path('<int:question_id>/', views.detail, name="detail")
     # ---------------------------------------------------------------------------------------- #
 ]
