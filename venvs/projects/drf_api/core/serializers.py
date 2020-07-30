0821780646a89feb3212ea1e532c7f7005430e26
jinho6225 <jinho6225@hotmail.com>
2020-07-30 12:10:49 -0700

    Django restframework api

---
+++ venvs/projects/drf_api/core/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from .models import Post
+
+# from django import forms
+
+# class PostForm(forms.modelForm):
+#     class Meta:
+#         model = Post
+#         fields = (
+#             'title', 'description'
+#         )
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = (
+            'title', 
+            'description',
+            'owner'
+        )
---
+++ venvs/projects/drf_api/core/views.py
@@ -0,0 +1,25 @@
+from django.http import JsonResponse
+from django.shortcuts import render
+
+# third party imports
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .serializers import PostSerializer
+from .models import Post
+
+
+class TestView(APIView):
+    def get(self, request, *args, **kwargs):
+        qs = Post.objects.all()
+        # post = qs.first()
+        serializer = PostSerializer(qs, many=True)
+        # serializer = PostSerializer(post)
+        return Response(serializer.data)
+
+    def post(self, request, *args, **kwargs):
+      serializer = PostSerializer(data=request.data)
+      if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+      return Response(serializer.errors)
